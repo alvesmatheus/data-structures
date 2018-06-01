98744c7e35d12a032a268ec21cbb84b8602d1524
SpinnelSun <m.hollygard@gmail.com>
2018-06-01 17:01:44 -0300

    Adição da Circular Queue

---
+++ src/queue/CircularQueue.java
@@ -0,0 +1,157 @@
+package queue;
+
+import abstractStructures.Queue;
+import exceptions.QueueOverflowException;
+import exceptions.QueueUnderflowException;
+
+/**
+ * A queue is a First-In-First-Out (FIFO) data structure which elements are kept
+ * in order and which main operations are enqueue and dequeue. In a FIFO data
+ * structure, the first element added will be the first one to be removed. A
+ * circular queue is an optimization of the basic queue that guarantees that
+ * every operation will be done in O(1).
+ * 
+ * @author Matheus Alves dos Santos
+ * 
+ */
+public class CircularQueue<T> implements Queue<T> {
+
+	private T[] array;
+	private int tail;
+	private int head;
+	private int elements;
+
+	/**
+	 * Constructs a CircularQueue from the given size parameter. This CircularQueue
+	 * is initially empty, so its tail and head starts as -1, while its elements are
+	 * initially 0. Also, this queue will be able to contain and operate with
+	 * T-typed objects. The size must be positive.
+	 * 
+	 * @param size
+	 *            The size of the new CircularQueue.
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	public CircularQueue(int size) {
+		if (size >= 1) {
+			array = (T[]) new Object[size];
+			head = -1;
+			tail = -1;
+			elements = 0;
+		}
+	}
+
+	/**
+	 * This method validates a element received by the queue. The element must not
+	 * be null to the queue operations work properly.
+	 * 
+	 * @param element
+	 *            The element that must be used in a queue operation.
+	 * 
+	 * @return the boolean that represents the element validity.
+	 * 
+	 */
+	private boolean isValidInput(T element) {
+		return (element != null);
+	}
+	
+	/**
+	 * Returns true, if the queue is empty, or false, otherwise.
+	 * 
+	 * @return the boolean meaning if the queue is empty.
+	 * 
+	 */
+	@Override
+	public boolean isEmpty() {
+		return (this.elements == 0);
+	}
+
+	/**
+	 * Returns true, if the queue is full, or false, otherwise.
+	 * 
+	 * @return the boolean meaning if the queue is full.
+	 * 
+	 */
+	@Override
+	public boolean isFull() {
+		return (this.elements == this.array.length);
+	}
+
+	/**
+	 * Returns (without removing) the oldest element of the queue or null if the
+	 * queue is empty.
+	 * 
+	 * @return the first element of the queue (or null if the queue is empty).
+	 * 
+	 */
+	@Override
+	public T head() {
+		T head = null;
+
+		if (!this.isEmpty()) {
+			head = this.array[this.head];
+		}
+
+		return head;
+	}
+	
+	/**
+	 * Inserts a new element in the queue or returns an exception if the queue is
+	 * already full. Null elements are not allowed (the queue remains unchanged).
+	 * 
+	 * @param element
+	 *            The element that must be enqueued.
+	 * 
+	 * @throws QueueOverflowException
+	 * 
+	 */
+	@Override
+	public void enqueue(T element) throws QueueOverflowException {
+		if (this.isFull()) {
+			throw new QueueOverflowException();
+		}
+
+		if (this.isValidInput(element)) {
+			if ((this.head == -1) && (this.tail == -1)) {
+				this.head++;
+				this.tail++;
+			}
+
+			else {
+				this.tail = (this.tail + 1) % this.array.length;
+			}
+
+			this.array[this.tail] = element;
+			this.elements++;
+		}
+	}
+
+	/**
+	 * If the queue has elements, it removes the oldest one in the queue and returns
+	 * it. Otherwise, it throws an exception.
+	 * 
+	 * @return the dequeued element.
+	 * 
+	 * @throws QueueUnderflowException
+	 * 
+	 */
+	@Override
+	public T dequeue() throws QueueUnderflowException {
+		if (this.isEmpty()) {
+			throw new QueueUnderflowException();
+		}
+
+		T dequeued = this.head();
+		this.elements--;
+
+		if (this.head == this.tail) {
+			this.head = -1;
+			this.tail = -1;
+		} else {
+			this.head = (this.head + 1) % this.array.length;
+		}
+
+		return dequeued;
+	}
+
+}
